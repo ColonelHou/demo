049afdaa7bed76cac4c2c2f32c647e2ebfa21de8
houningning <houningning@yiche.com>
2020-03-24 09:13:11 +0800

    CEP实例

--- src/main/scala/com/flink/course/FlinkCEPExample.scala
+++ src/main/scala/com/flink/course/FlinkCEPExample.scala
@@ -10,22 +10,29 @@ import scala.collection.Map
 
 object FlinkCEPExample {
 
-  case class LoginEvent(userId: String, ip: String, eventType: String, eventTime: String)
+  case class LoginEvent(id: String, username: String, eventType: String, eventTime: String)
 
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
+    /**
+     * 结果：
+     * 1. 用户名:张三，登录时间:1577080457,1577080458,1577080460
+     *    用户名:张三, 登录时间:1577080458,1577080460,1577080462
+     */
     val stream = env
       .fromElements(
-        LoginEvent("1", "192.168.0.1", "fail", "1"),
-        LoginEvent("1", "192.168.0.2", "fail", "2"),
-        LoginEvent("1", "192.168.0.3", "fail", "3"),
-        LoginEvent("2", "192.168.0.4", "success", "4")
+        LoginEvent("1", "张三", "fail", "1577080457"),
+        LoginEvent("2", "张三", "fail", "1577080458"),
+        LoginEvent("3", "张三", "fail", "1577080460"),
+        LoginEvent("4", "李四", "fail", "1577080458"),
+        LoginEvent("5", "李四", "success", "1577080462"),
+        LoginEvent("6", "张三", "fail", "1577080462")
       )
       .assignAscendingTimestamps(_.eventTime.toLong * 1000)
-      .keyBy(_.userId)
+      .keyBy(_.id)
 
     val pattern = Pattern
       .begin[LoginEvent]("first").where(_.eventType == "fail")
@@ -41,7 +48,7 @@ object FlinkCEPExample {
         val second = pattern.getOrElse("second", null).iterator.next()
         val third = pattern.getOrElse("third", null).iterator.next()
 
-        "userId是： " + first.userId + " 的用户， 10s 之内连续登录失败了三次，" + "ip地址分别是： " + first.ip + "; " + second.ip + "; " + third.ip
+        "userId是： " + first.username + " 的用户， 10s 之内连续登录失败了三次，" + "ip地址分别是： " + first.eventTime + "; " + second.eventTime + "; " + third.eventTime
       })
 
     loginFailStream.print()
